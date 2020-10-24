c9a58518dae455be35d1cd05fe52b9b5c7d7b9cc
jan hof <>
2020-10-24 20:38:43 +0200

    initial template setup

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -5,8 +5,8 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
-        <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        <a href="http://sauer-bier.de" target="_blank" rel="noopener noreferrer">HueX</a>
+        <span className="ml-1">&copy; 2020 MedicyX ltd.</span>
       </div>
       <div className="mfs-auto">
         <span className="mr-1">Powered by</span>
